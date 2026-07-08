99677bc66f92c3fbbf517dadd39989c8a689ebf9
Merge Analysis <merge-bot@local>
2026-07-08 05:14:08 +0000

    Wire real Google OAuth Android client ID

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,2 +1,2 @@
 export const BACKEND_URL = 'https://tulip-backend-fcrw.onrender.com';
-export const GOOGLE_CLIENT_ID = ''; // TODO: Replace with real Google Client ID
+export const GOOGLE_CLIENT_ID = '195981996582-p1dr4sadsuiemg5petnbk8gvtn0h0nng.apps.googleusercontent.com';
